e6660dbc732db6a10348ad0dc2d8cbd104edc0e8
gapittm <gapittm@clemson.edu>
2017-04-09 20:03:22 -0400

    permissions

--- software/mpi_python_install.sh
+++ software/mpi_python_install.sh
@@ -3,7 +3,7 @@ wget https://www.open-mpi.org/software/ompi/v1.10/downloads/openmpi-1.10.3.tar.g
 wget https://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh
 
 # run anaconda installation script
-bash Anaconda3-4.3.1-Linux-x86_64.sh -b -p /packages/anaconda3
+sudo bash Anaconda3-4.3.1-Linux-x86_64.sh -b -p /packages/anaconda3
 
 # install mpi
 tar -xvf openmpi-1.10.3.tar.gz
